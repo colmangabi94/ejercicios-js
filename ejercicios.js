7318df97682446dbc6bd6848304e419d730162c0
colmangabi94 <colmangabi94@gmail.com>
2024-10-08 19:11:15 -0300

    v1

---
+++ ejercicios.js
@@ -0,0 +1,42 @@
+// Ejercicio 1: Comparar dos números
+function Compararnúmeros (num1, num2)  {
+
+//Comparar si el numero num1 es mayor que num2
+if (num1 > num2) }
+console.log (num1 + " es mayor que " + num2);
+} else if (num1 < num2) {
+    console.log(num1 + " es menor que " + num2);
+} else {
+    console.log(num1 + " es igual a " + num2);
+}
+
+//Ejercicio 2: Operadores logicos
+function Operadores logicos (AND, OR, NOT)
+{ true && true// true
+    true && false//  false
+    false && true//  false  
+    }
+
+//Ejercicio 3: Variables
+function Variables (let)
+{ let camelCase = 1
+    let camelCaseIsCool = 2
+    let username = `Gabbie`
+    let  password = `123456`
+    console.log(username + "Gabbie" + password);
+    console.log(camelCaseIsCool + "2");
+    console.log(camelCase + "1");
+    console.log(username + "Gabbie");
+    console.log(password + "123456");
+    }
+
+    //Ejercicio 4: Tipo de datos
+    function Tipos de datos (string, number, boolean, null, undefined, objeto, array,
+        {function string() {'Estás aprendiendo ' + 'JavaScript'  // 'Estás aprendiendo JavaScript' }
+            function number (2 + 2) * 3   // 8
+            (2 + 2) * 3 // 12) 
+console.log (string( 2 + 2) * 3)  // 12
+}
+
+
+
